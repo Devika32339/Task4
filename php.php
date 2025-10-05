cf4b1c27742da3bdfacae2bd27393451a713307a
Devika32339 <devikaa20112002@gmail.com>
2025-10-05 16:43:40 +0530

    Add files via upload

---
+++ php.php
@@ -0,0 +1,19 @@
+<?php
+// Set the name variable
+$name = "Arjun"; // You can change this to your name
+
+// Get the current date and time
+$current_time = date('Y-m-d H:i:s');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello PHP</title>
+</head>
+<body>
+
+    <h1>Hello <?php echo $name; ?>, right now the time is <?php echo $current_time; ?></h1>
+
+</body>
+</html>
---
+++ rust.rs
@@ -0,0 +1,16 @@
+// Import the necessary modules from the chrono crate
+use chrono::{Local, Timelike};
+
+fn main() {
+    // Get the current local date and time
+    let now = Local::now();
+
+    // Get the name from the environment or use a default
+    let name = "Arjun"; // You can change this to your name
+
+    // Format the time as a string
+    let formatted_time = now.format("%Y-%m-%d %H:%M:%S").to_string();
+
+    // Print the greeting with the name and formatted time
+    println!("Hello {}, right now the time is {}", name, formatted_time);
+}
